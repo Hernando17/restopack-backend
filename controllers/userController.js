33a4af3c4bcfd60b8b2e70c64e0a76a63576b468
Hernando17 <ginkotsu17@gmail.com>
2023-08-19 22:00:53 +0700

    user information update

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,4 +39,36 @@ async function getUserById(req, res, next) {
   }
 }
 
-module.exports = { getAllUser, getUserById };
+async function updateUserInformation(req, res, next) {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  try {
+    await Users.update(
+      {
+        name: name,
+      },
+      {
+        where: {
+          id: parseInt(id),
+        },
+      }
+    ).then(async function (user) {
+      if (user) {
+        return res.status(200).json({
+          head: "Success",
+          message: "User updated",
+        });
+      } else {
+        return res.status(404).json({
+          head: "Failed",
+          message: "User not found",
+        });
+      }
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = { getAllUser, getUserById, updateUserInformation };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,8 @@
-const { getAllUser, getUserById } = require("../controllers/userController.js");
+const {
+  getAllUser,
+  getUserById,
+  updateUserInformation,
+} = require("../controllers/userController.js");
 const { tokenAuthenticate } = require("../middlewares/tokenAuthentication");
 const express = require("express");
 const router = express.Router();
@@ -6,7 +10,11 @@ const url = `api/${process.env.VERSION}`;
 
 router.get(`/${url}/user/:id`, tokenAuthenticate, getUserById);
 router.get(`/${url}/userlist`, tokenAuthenticate, getAllUser);
-// router.patch("/updateuser/:id", updateUser);
+router.patch(
+  `/${url}/updateuserinformation/:id`,
+  tokenAuthenticate,
+  updateUserInformation
+);
 // router.delete("/deleteuser/:id", deleteUser);
 
 module.exports = router;
