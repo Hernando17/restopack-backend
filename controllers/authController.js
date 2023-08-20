d64ee9da20d8b85678522384aac2020a789c3531
Hernando17 <ginkotsu17@gmail.com>
2023-08-20 14:08:52 +0700

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,98 +3,100 @@ const bcrypt = require("bcrypt");
 const { Users } = require("../models");
 
 async function userRegister(req, res, next) {
-  try {
-    const { name, email, password, confirm_password, isRestaurant } = req.body;
+    try {
+        const { name, email, password, confirm_password, isRestaurant } =
+            req.body;
 
-    Users.findOne({
-      where: {
-        email,
-      },
-    }).then((response) => {
-      if (response) {
-        return res.status(409).json({
-          head: "Failed",
-          message: "Email has been used",
+        Users.findOne({
+            where: {
+                email,
+            },
+        }).then((response) => {
+            if (response) {
+                return res.status(409).json({
+                    head: "Failed",
+                    message: "Email has been used",
+                });
+            }
         });
-      }
-    });
 
-    if (password !== confirm_password) {
-      return res
-        .status(400)
-        .json({ head: "Failed", message: "Password doesn't match" });
+        if (password !== confirm_password) {
+            return res
+                .status(400)
+                .json({ head: "Failed", message: "Password doesn't match" });
+        }
+
+        const saltRounds = 10;
+        const salt = bcrypt.genSaltSync(saltRounds);
+
+        await Users.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password, salt),
+            isRestaurant,
+        }).then(function () {
+            return res.status(201).send({
+                head: "Success",
+                message: "User registration successfully",
+            });
+        });
+    } catch (error) {
+        next(error);
     }
-
-    const saltRounds = 10;
-    const salt = bcrypt.genSaltSync(saltRounds);
-
-    await Users.create({
-      name,
-      email,
-      password: bcrypt.hashSync(password, salt),
-      isRestaurant,
-    }).then(function () {
-      return res
-        .status(201)
-        .send({ head: "Success", message: "User registration successfully" });
-    });
-  } catch (error) {
-    next(error);
-  }
 }
 
 async function userLogin(req, res, next) {
-  const { email, password } = req.body;
+    const { email, password } = req.body;
 
-  try {
-    Users.findOne({
-      where: {
-        email,
-      },
-    }).then(function (user) {
-      bcrypt.compare(password, user.password, function (err, result) {
-        const token = jwt.sign(
-          {
-            username: user.username,
-            email: user.email,
-          },
-          process.env.SECRET,
-          { expiresIn: "1d" }
-        );
-
-        const refreshToken = jwt.sign(
-          {
-            username: user.username,
-            email: user.email,
-          },
-          process.env.REFRESH_TOKEN_SECRET,
-          { expiresIn: "30d" }
-        );
-
-        if (result) {
-          return res.status(200).json({
-            head: "Success",
-            message: "User login successfully",
-            user: {
-              id: user.id,
-              username: user.username,
-              email: user.email,
-              isRestaurant: true,
+    try {
+        Users.findOne({
+            where: {
+                email,
             },
-            token,
-            refreshToken,
-          });
-        } else {
-          return res.status(403).json({
-            head: "Failed",
-            message: "Wrong password",
-          });
-        }
-      });
-    });
-  } catch (error) {
-    next(error);
-  }
+        }).then(function (user) {
+            bcrypt.compare(password, user.password, function (err, result) {
+                const token = jwt.sign(
+                    {
+                        username: user.username,
+                        email: user.email,
+                    },
+                    process.env.SECRET,
+                    { expiresIn: "1d" }
+                );
+
+                const refreshToken = jwt.sign(
+                    {
+                        username: user.username,
+                        email: user.email,
+                    },
+                    process.env.REFRESH_TOKEN_SECRET,
+                    { expiresIn: "30d" }
+                );
+
+                if (result) {
+                    return res.status(200).json({
+                        head: "Success",
+                        message: "User login successfully",
+                        user: {
+                            id: user.id,
+                            username: user.username,
+                            email: user.email,
+                            isRestaurant: true,
+                        },
+                        token,
+                        refreshToken,
+                    });
+                } else {
+                    return res.status(403).json({
+                        head: "Failed",
+                        message: "Wrong password",
+                    });
+                }
+            });
+        });
+    } catch (error) {
+        next(error);
+    }
 }
 
 module.exports = { userRegister, userLogin };
